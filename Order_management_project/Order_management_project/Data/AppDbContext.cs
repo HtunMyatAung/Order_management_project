62088e11f9ed1e3b33bd566cc013f8e6efe9de55
HtunMyatAung <htunmyataung643@gmail.com>
2024-06-05 10:44:46 +0630

    first

---
+++ Order_management_project/Order_management_project/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Order_management_project.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<>
+    }
+}
